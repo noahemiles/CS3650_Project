96d83d4a9f8207cacdf370887b6150be8d0d6286
Noah Miles <noahemiles@gmail.com>
2018-12-02 17:51:46 -0800

    removed  header

--- ScreenManagement.java
+++ ScreenManagement.java
@@ -1,22 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cs3650_project;
 
 import java.io.IOException;
 
-/**
- *
- * @author Johnawin
- */
 public class ScreenManagement {
-    
+
     public void clearScreen() {
         System.out.println(new String(new char[100]).replace("\0", "\r\n"));
     }
-    
+
     public void waitForInput() {
         try {
             System.in.read();
@@ -25,5 +16,5 @@ public class ScreenManagement {
             e.printStackTrace();
         }
     }
-    
+
 }
